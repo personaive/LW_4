bbc374fed656abad98539f1379a241a44b4b583f
personaive <nik.bachurin2704@mail.ru>
2026-03-19 20:24:37 +0500

    LW_4

---
+++ LW_4_1/Program.fs
@@ -0,0 +1,85 @@
+﻿open System
+
+type 't tree = 
+    Node of 't * 't tree * 't tree | Nil // обобщённый тип дерева
+
+let addDigitToNumber digit number =
+    if number >= 0 then
+        int (string digit + string number)
+    else
+        let absoluteString = string (-number)
+        int ("-" + string digit + absoluteString)
+
+let rec mapTree applyFunction currentTree =
+    match currentTree with
+    | Nil -> Nil
+    | Node(nodeValue, leftSubTree, rightSubTree) ->
+        Node(applyFunction nodeValue,
+             mapTree applyFunction leftSubTree,
+             mapTree applyFunction rightSubTree)
+
+let addDigitToTree digit originalTree = 
+    mapTree (addDigitToNumber digit) originalTree
+
+let rec insertNode currentTree numberToInsert =
+    match currentTree with
+    | Nil -> Node(numberToInsert, Nil, Nil)
+    | Node(nodeValue, leftSubTree, rightSubTree) ->
+        if numberToInsert < nodeValue then 
+            Node(nodeValue,
+            insertNode leftSubTree numberToInsert,rightSubTree)
+        else 
+            Node(nodeValue,
+            leftSubTree, insertNode rightSubTree numberToInsert)
+
+let rec printTree currentTree indent =
+    match currentTree with
+    | Nil -> ()
+    | Node(nodeValue, leftSubTree, rightSubTree) ->
+        printTree rightSubTree (indent + 4)
+        printfn "%s%d" (String(' ', indent)) nodeValue
+        printTree leftSubTree (indent + 4)
+
+let rec readDigit () =
+    printf "Введите цифру для добавления: "
+    let input = 
+        Console.ReadLine().Trim() // убираем лишние пробелы
+
+    if input.Length = 1 && Char.IsDigit(input.[0]) then 
+        int input // конвертируем в число
+    else
+        printfn "Некорректный ввод. Введите от 0 до 9."
+        readDigit ()
+
+[<EntryPoint>]
+let main argv =
+
+    let randomGenerator = 
+        Random() // генератор случайных чисел
+
+    printf "Сколько чисел в дереве?"
+    let numberCount = 
+        Console.ReadLine() |> int
+
+    let numbersList = 
+        [ for i in 1..numberCount ->
+            randomGenerator.Next(-150, 160) ] // создаём список
+
+    printfn "Список чисел: %A" numbersList
+
+    let tree = 
+        List.fold insertNode Nil numbersList
+
+    printfn "\nИсходное дерево:"
+    printTree tree 0 // вывод исходного дерева
+
+    let digitToAdd =
+        readDigit () // ввод цифры для добавления
+
+    let newTree =
+        addDigitToTree digitToAdd tree
+
+    printfn "\nНовое дерево:"
+    printTree newTree 0
+
+    0
---
+++ LW_4_2/Program.fs
@@ -0,0 +1,88 @@
+﻿open System
+
+type 't tree = 
+    Node of 't * 't tree * 't tree | Nil 
+
+let rec insertNode currentTree numberToInsert =
+    match currentTree with
+    | Nil -> Node(numberToInsert, Nil, Nil)
+    | Node(nodeValue, leftSubtree, rightSubtree) ->
+        if numberToInsert < nodeValue then 
+            Node(nodeValue,
+            insertNode leftSubtree numberToInsert, rightSubtree)
+        else 
+            Node(nodeValue,
+            leftSubtree, insertNode rightSubtree numberToInsert)
+
+let rec printTree currentTree indent =
+    match currentTree with
+    | Nil -> ()
+    | Node(nodeValue, leftSubtree, rightSubtree) ->
+        printTree rightSubtree (indent + 4)
+        printfn "%s%d" (String(' ', indent)) nodeValue
+        printTree leftSubtree (indent + 4)
+
+let rec treeFold folder accumulator currentTree =
+    match currentTree with
+    | Nil -> accumulator
+    | Node(nodeValue, leftSubtree, rightSubtree) ->
+        let leftAcc =
+            treeFold folder accumulator leftSubtree
+
+        let currentAcc =
+            folder leftAcc nodeValue
+
+        treeFold folder currentAcc rightSubtree
+
+let countByDigits currentCount number requiredDigits =
+    let digits =
+        number.ToString().Length
+
+    if digits =
+        requiredDigits then 
+            currentCount + 1
+    else
+        currentCount
+
+let rec readNaturalNumber promptMessage =
+    printf "%s" promptMessage
+
+    let input =
+        Console.ReadLine().Trim()
+
+    match System.Int32.TryParse(input) with
+    | true, value when value > 0 -> value
+    | _ ->
+        printfn "Ошибка! Введите натуральное число больше 0."
+        readNaturalNumber promptMessage
+
+[<EntryPoint>]
+let main argv =
+
+    let random =
+        Random()
+
+    let numberCount =
+        readNaturalNumber "Сколько чисел в дереве? "
+
+    let numbersList =
+        [ for i in 1..numberCount -> random.Next(1, 1000) ]
+    printfn "Список чисел: %A" numbersList
+
+    let tree =
+        List.fold insertNode Nil numbersList
+    printfn "\nИсходное дерево:"
+    printTree tree 0
+
+    let requiredDigits =
+        readNaturalNumber "\nВведите значность для подсчёта: "
+
+    let count =
+        treeFold (fun acc value -> 
+            countByDigits acc value requiredDigits)
+            0 tree
+
+    printfn "\nКоличество чисел с %d цифрами" requiredDigits
+    printfn "в дереве: %d" count
+
+    0
