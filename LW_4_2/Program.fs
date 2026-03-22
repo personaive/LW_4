137907f17fe102e16380edd06fc846d0d1ebfa23
personaive <nik.bachurin2704@mail.ru>
2026-03-22 23:12:51 +0500

    Update Program.fs

--- LW_4_2/Program.fs
+++ LW_4_2/Program.fs
@@ -1,4 +1,4 @@
-﻿open System
+open System
 
 type 't tree = 
     Node of 't * 't tree * 't tree | Nil 
@@ -9,10 +9,10 @@ let rec insertNode currentTree numberToInsert =
     | Node(nodeValue, leftSubtree, rightSubtree) ->
         if numberToInsert < nodeValue then 
             Node(nodeValue,
-            insertNode leftSubtree numberToInsert, rightSubtree)
+                 insertNode leftSubtree numberToInsert, rightSubtree)
         else 
             Node(nodeValue,
-            leftSubtree, insertNode rightSubtree numberToInsert)
+                 leftSubtree, insertNode rightSubtree numberToInsert)
 
 let rec printTree currentTree indent =
     match currentTree with
@@ -38,44 +38,62 @@ let countByDigits currentCount number requiredDigits =
     let digits =
         number.ToString().Length
 
-    if digits =
-        requiredDigits then 
-            currentCount + 1
+    if digits = requiredDigits then 
+        currentCount + 1
     else
         currentCount
 
-let rec readNaturalNumber promptMessage =
-    printf "%s" promptMessage
+let rec readCount () =
+    printf "Сколько чисел в дереве? "
+    let input = Console.ReadLine().Trim()
 
-    let input =
-        Console.ReadLine().Trim()
-
-    match System.Int32.TryParse(input) with
-    | true, value when value > 0 -> value
+    try
+        let value = int input
+        if value > 0 then value
+        else
+            printfn "Введите число больше 0!"
+            readCount ()
+    with
     | _ ->
-        printfn "Ошибка! Введите натуральное число больше 0."
-        readNaturalNumber promptMessage
+        printfn "Некорректный ввод!"
+        readCount ()
+
+let rec readDigits () =
+    printf "Введите значность для подсчёта: "
+    let input = Console.ReadLine().Trim()
+
+    try
+        let value = int input
+        if value > 0 then value
+        else
+            printfn "Введите число больше 0!"
+            readDigits ()
+    with
+    | _ ->
+        printfn "Некорректный ввод!"
+        readDigits ()
 
 [<EntryPoint>]
 let main argv =
 
-    let random =
-        Random()
+    let random = Random()
 
     let numberCount =
-        readNaturalNumber "Сколько чисел в дереве? "
+        readCount ()
 
     let numbersList =
         [ for i in 1..numberCount -> random.Next(1, 1000) ]
+
     printfn "Список чисел: %A" numbersList
 
     let tree =
         List.fold insertNode Nil numbersList
+
     printfn "\nИсходное дерево:"
     printTree tree 0
 
     let requiredDigits =
-        readNaturalNumber "\nВведите значность для подсчёта: "
+        readDigits ()
 
     let count =
         treeFold (fun acc value -> 
